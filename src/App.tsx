36a1783a8091e7bf1f7a87bcccbad79ccb097322
c43721 <rlahman.game@gmail.com>
2020-12-10 19:37:50 -0600

    Create a dark theme and customize it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import getTheme from './themes/theme';
 import './App.css';
 
 //Routes
@@ -7,16 +11,21 @@ import HomePage from './pages/HomePage';
 
 const TestPage = lazy(() => import('./pages/TestPage'));
 
+const theme = getTheme();
+
 function App() {
   return (
-    <React.Suspense fallback={<span>Loading</span>}>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/test' component={TestPage} />
-        </Switch>
-      </Router>
-    </React.Suspense>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <React.Suspense fallback={<span>Loading</span>}>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/test' component={TestPage} />
+          </Switch>
+        </Router>
+      </React.Suspense>
+    </ThemeProvider>
   );
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,34 @@
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid/Grid';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Layout from '../components/Layout';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '80vh',
+  },
+}));
+
 export default function HomePage() {
+  const styles = useStyles();
+
   return (
-    <div>
-      <Link to='/test'>
-          Test
-      </Link>
-      Hello Wolrd
-    </div>
+    <Layout>
+      <Grid
+        className={styles.root}
+        container
+        justify='center'
+        alignItems='center'
+        direction='column'
+      >
+        <Typography variant='h3'>rcon.tf</Typography>
+        <Typography variant='subtitle1'>
+          The platform to manage all your server needs.
+        </Typography>
+      </Grid>
+    </Layout>
   );
 }
