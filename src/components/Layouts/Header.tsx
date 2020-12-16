7e6d99be977f0744d028c356c652fef581beccbc
c43721 <rlahman.game@gmail.com>
2020-12-15 18:46:34 -0600

    Style the menu so that it can be used for routes

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,6 +9,9 @@ import { useSelector } from 'react-redux';
 import { userSelector } from '../../redux/users/userSlice';
 import IconButton from '@material-ui/core/IconButton';
 import { Avatar } from '@material-ui/core';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,6 +23,10 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  menuLink: {
+    color: "#FFF",
+    textDecoration: "none",
+  }
 }));
 
 interface HeaderProps {
@@ -32,6 +39,16 @@ export default function Header({ login, logout }: HeaderProps) {
 
   const user = useSelector(userSelector);
 
+	const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const handleMenuOpen  = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position='static'>
@@ -41,17 +58,44 @@ export default function Header({ login, logout }: HeaderProps) {
           </Typography>
 
           {user?.id?.length ? (
-            <div>
+						<>
               <IconButton
-                aria-label='account of current user'
-                aria-controls='menu-appbar'
-                aria-haspopup='true'
-                onClick={logout}
-                color='inherit'
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenuOpen}
+                color="inherit"
               >
-                <Avatar src={user.avatar} />
+                 <Avatar src={user.avatar} />
               </IconButton>
-            </div>
+							<Menu
+								id="simple-menu"
+								anchorEl={anchorEl}
+								keepMounted
+								open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+							>
+								<MenuItem>
+									<Link to="/" className={classes.menuLink}>
+										Home
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link to="/dashboard" className={classes.menuLink}>
+										Servers
+									</Link>
+								</MenuItem>
+								<MenuItem onClick={() => logout()}>Logout</MenuItem>
+							</Menu>
+						</>
           ) : (
             <Button variant='text' onClick={login}>
               Login
