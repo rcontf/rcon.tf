4e582520f73f23c7af1f68a2bbb448eb5b186d2d
c43721 <rlahman.game@gmail.com>
2020-12-15 19:58:14 -0600

    Modal for adding a server, add form with some validation

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import Layout from '../components/Layouts/Layout';
 import axios from 'axios';
 import { GetServerResponse } from '../types/types';
@@ -8,17 +8,45 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   root: {
     marginTop: '5vh',
+  },
+  paper: {
+    position: 'absolute',
+    width: 400,
+    height: 600,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
 }));
 
 export default function DashboardPage() {
   const classes = useStyles();
 
+  const [open, setOpen] = useState<boolean>(false);
   const [servers, setServers] = useState<GetServerResponse[]>([]);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('submitted');
+  };
 
   useEffect(() => {
     axios.get<GetServerResponse[]>('/servers').then(({ data }) => {
@@ -37,11 +65,56 @@ export default function DashboardPage() {
           spacing={3}
         >
           <Typography variant='h4'>Select a Server</Typography>
-          <Button variant="outlined">
-              Add
+          <Button variant='outlined' onClick={handleOpen}>
+            Add
           </Button>
-          {servers && servers.map(server => <Grid item>{server.hostname}</Grid>)}
+          {servers &&
+            servers.map(server => (
+              <Grid item key={server.hostname}>
+                {server.hostname}
+              </Grid>
+            ))}
         </Grid>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='simple-modal-title'
+          aria-describedby='simple-modal-description'
+        >
+          <Paper className={classes.paper}>
+            <h2 id='simple-modal-title'>Add server</h2>
+            <form autoComplete='off' onSubmit={handleSubmit}>
+              <Grid
+                container
+                alignItems='center'
+                justify='center'
+                direction='column'
+                spacing={1}
+              >
+                <TextField
+                  id='server-hostname'
+                  label='Alias'
+                  helperText='What do you want to call the server?'
+                  fullWidth
+                  required
+                />
+                <TextField id='server-ip' label='IP' fullWidth required />
+                <TextField
+                  id='server-password'
+                  label='Rcon Password'
+                  fullWidth
+                  required
+                />
+                <Grid item>
+                  <Button variant='outlined' type='submit'>
+                    Add server
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Paper>
+        </Modal>
       </Container>
     </Layout>
   );
