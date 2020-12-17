1c76ee341605c1edba11811b687d502c0aa27363
c43721 <rlahman.game@gmail.com>
2020-12-17 00:13:32 -0600

    Edit and delete forms on dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../components/Layouts/Layout';
 import { RootState } from '../redux/store';
 import axios from 'axios';
@@ -7,14 +7,19 @@ import Player from '../components/Player';
 
 import {
   Box,
+  Button,
   Container,
   Grid,
   IconButton,
+  Modal,
+  Paper,
+  TextField,
   Typography,
 } from '@material-ui/core';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { useHistory } from 'react-router-dom';
+import { setServer } from '../redux/servers/serverSlice';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,6 +27,17 @@ const useStyles = makeStyles(theme => ({
   },
   playerContainer: {
     width: '100%',
+  },
+  modal: {
+    position: 'absolute',
+    width: 600,
+    height: 600,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
 }));
 
@@ -37,11 +53,64 @@ interface PlayerObject {
 
 export default function DashboardPage() {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const server = useSelector((state: RootState) => state.server);
   const history = useHistory();
 
   const [serverStats, setServerStats] = useState<string>('');
   const [players, setPlayers] = useState<PlayerObject[]>([]);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const serverIp = useRef<HTMLInputElement>();
+  const serverPassword = useRef<HTMLInputElement>();
+  const serverPort = useRef<HTMLInputElement>();
+  const serverHostname = useRef<HTMLInputElement>();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = () => {
+    const wantToDelete = window.confirm('Do you want to delete this server?');
+
+    if (!wantToDelete) return;
+    axios
+      .delete(`/servers/${server.info.ip}`)
+      .then(() => {
+        history.goBack();
+      })
+      .catch(er => console.log('Cannot reach server.\n' + er));
+  };
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!serverIp || !serverPassword || !serverPort) return;
+    else {
+      axios
+        .patch(`/servers/${server.info.ip}`, {
+          ip: serverIp!.current!.value,
+          password: serverPassword!.current!.value,
+          port: parseInt(serverPort!.current!.value),
+          hostname: serverHostname!.current!.value,
+        })
+        .then(({ data }) => {
+          dispatch(
+            setServer({
+              selected: serverHostname!.current!.value,
+              info: {
+                ip: serverIp!.current!.value,
+                password: serverPassword!.current!.value,
+                port: parseInt(serverPort!.current!.value),
+              },
+            })
+          );
+        });
+    }
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -122,10 +191,88 @@ export default function DashboardPage() {
                 </Grid>
               ))
             ) : (
-              <Grid item>No players</Grid>
+              <Grid item>
+                <Typography variant='h6' className={classes.root}>
+                  No players
+                </Typography>
+              </Grid>
             )}
           </Grid>
+          <Typography variant='h4' className={classes.root}>
+            Server Controls
+          </Typography>
+          <Button
+            variant='outlined'
+            onClick={async () => await sendCommand('_restart')}
+          >
+            Restart Server
+          </Button>
+          <Typography variant='h4' className={classes.root}>
+            Settings
+          </Typography>
+          <Button variant='outlined' onClick={() => handleDelete()}>
+            Delete
+          </Button>
+          <Button variant='outlined' onClick={() => handleOpen()}>
+            Edit
+          </Button>
         </Grid>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='edit-title'
+          aria-describedby='edit-description'
+        >
+          <Paper className={classes.modal}>
+            <h2 id='edit-title'>Edit</h2>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                id='server-ip'
+                label='Server Ip'
+                style={{ margin: 8 }}
+                fullWidth
+                margin='normal'
+                inputRef={serverIp}
+                defaultValue={server.info.ip}
+              />
+              <TextField
+                id='server-hostname'
+                label='Hostname'
+                style={{ margin: 8 }}
+                fullWidth
+                margin='normal'
+                inputRef={serverHostname}
+                defaultValue={server.selected}
+              />
+              <TextField
+                id='server-rcon-password'
+                label='Rcon Password'
+                style={{ margin: 8 }}
+                fullWidth
+                margin='normal'
+                inputRef={serverPassword}
+                defaultValue={server.info.password}
+              />
+              <TextField
+                id='server-rcon-port'
+                label='Port'
+                style={{ margin: 8 }}
+                fullWidth
+                type='number'
+                margin='normal'
+                inputRef={serverPort}
+                defaultValue={server.info.port}
+              />
+
+              <Button variant='text' type='submit'>
+                Edit
+              </Button>
+              <Button variant='text' type='reset'>
+                Reset
+              </Button>
+            </form>
+          </Paper>
+        </Modal>
       </Container>
     </Layout>
   );
