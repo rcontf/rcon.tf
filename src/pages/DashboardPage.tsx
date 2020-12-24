805d94e81bed62751e8595875d886be7bdee897d
c43721 <rlahman.game@gmail.com>
2020-12-24 01:46:40 -0600

    Remove form for editing server, add placeholder map + player count

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Layout from '../components/Layouts/Layout';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import {
-  deleteServer,
-  editServer,
-  serverSelector,
-} from '../redux/servers/serverSlice';
+import { serverSelector } from '../redux/servers/serverSlice';
 import SteamId from 'steamid';
 
 import {
@@ -16,9 +11,7 @@ import {
   Container,
   Grid,
   IconButton,
-  Modal,
   Paper,
-  TextField,
   Typography,
   TableCell,
   TableContainer,
@@ -31,7 +24,6 @@ import {
 import DeleteIcon from '@material-ui/icons/Delete';
 import BlockIcon from '@material-ui/icons/Block';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -39,18 +31,6 @@ const useStyles = makeStyles(theme => ({
   },
   playerContainer: {
     width: '100%',
-  },
-  modal: {
-    position: 'absolute',
-    width: 600,
-    height: 600,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    outline: 0,
   },
   table: {
     minWidth: 650,
@@ -69,51 +49,26 @@ interface PlayerObject {
   connected: string;
 }
 
+interface ServerDetails {
+  map: string;
+  players: string;
+}
+
 export default function DashboardPage() {
   const classes = useStyles();
-  const dispatch = useDispatch();
   const server = useSelector(serverSelector);
-  const history = useHistory();
 
-  const [, setServerStats] = useState<string>('');
+  const [serverStats, setServerStats] = useState('');
+  const [serverDetails, setServerDetails] = useState<ServerDetails>({
+    map: '',
+    players: '0/24',
+  });
   const [players, setPlayers] = useState<PlayerObject[]>([]);
-  const [open, setOpen] = useState<boolean>(false);
 
-  const serverIp = useRef<HTMLInputElement>();
-  const serverPassword = useRef<HTMLInputElement>();
-  const serverPort = useRef<HTMLInputElement>();
-  const serverHostname = useRef<HTMLInputElement>();
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleDelete = () => {
-    const wantToDelete = window.confirm('Do you want to delete this server?');
-
-    if (!wantToDelete) return;
-    dispatch(deleteServer(server.selected.ip));
-    history.goBack();
-  };
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    if (!serverIp || !serverPassword || !serverPort) return;
-    else {
-      dispatch(
-        editServer(server.selected.ip, {
-          hostname: serverHostname!.current!.value,
-          ip: serverIp!.current!.value,
-          password: serverPassword!.current!.value,
-          port: parseInt(serverPort!.current!.value),
-        })
-      );
-    }
-  }
+  useEffect(() => {
+    const updatedDetails = getServerDetails(serverStats);
+    setServerDetails(updatedDetails);
+  }, [serverStats]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -173,11 +128,15 @@ export default function DashboardPage() {
     <Layout>
       <Container className={classes.root}>
         <Grid container justify='center' alignItems='center' direction='column'>
-          <Box display='flex' justifyContent='center' alignItems='center'>
-            <IconButton onClick={() => history.goBack()}>
-              <ArrowBackIcon />
-            </IconButton>
+          <Box
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            flexDirection='column'
+          >
             <Typography variant='h4'>{server.selected.hostname}</Typography>
+            <Typography variant='h6'>{serverDetails.map}</Typography>
+            <Typography variant='h6'>{serverDetails.players}</Typography>
           </Box>
 
           <Typography variant='h4' className={classes.root}>
@@ -267,80 +226,7 @@ export default function DashboardPage() {
               Kick all
             </Button>
           </Box>
-
-          <Typography variant='h4' className={classes.root}>
-            Settings
-          </Typography>
-          <Box
-            display='flex'
-            justifyContent='center'
-            alignItems='center'
-            gridGap={10}
-          >
-            <Button variant='outlined' onClick={() => handleDelete()}>
-              Delete
-            </Button>
-            <Button variant='outlined' onClick={() => handleOpen()}>
-              Edit
-            </Button>
-          </Box>
         </Grid>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby='edit-title'
-          aria-describedby='edit-description'
-        >
-          <Paper className={classes.modal}>
-            <h2 id='edit-title'>Edit</h2>
-            <form onSubmit={handleSubmit}>
-              <TextField
-                id='server-hostname'
-                label='Hostname'
-                style={{ margin: 8 }}
-                fullWidth
-                margin='normal'
-                inputRef={serverHostname}
-                defaultValue={server.selected.hostname}
-              />
-              <TextField
-                id='server-ip'
-                label='Server Ip'
-                style={{ margin: 8 }}
-                fullWidth
-                margin='normal'
-                inputRef={serverIp}
-                defaultValue={server.selected.ip}
-              />
-              <TextField
-                id='server-rcon-password'
-                label='Rcon Password'
-                style={{ margin: 8 }}
-                fullWidth
-                margin='normal'
-                inputRef={serverPassword}
-                defaultValue={server.selected.password}
-              />
-              <TextField
-                id='server-rcon-port'
-                label='Port'
-                style={{ margin: 8 }}
-                fullWidth
-                type='number'
-                margin='normal'
-                inputRef={serverPort}
-                defaultValue={server.selected.port}
-              />
-
-              <Button variant='text' type='submit'>
-                Edit
-              </Button>
-              <Button variant='text' type='reset'>
-                Reset
-              </Button>
-            </form>
-          </Paper>
-        </Modal>
       </Container>
     </Layout>
   );
@@ -354,7 +240,9 @@ function getPlayers(body: string): PlayerObject[] {
   if (!players.length) return [];
 
   return players.map(player => {
-    const playerDetailsArrayRaw = player.match(/[^\s"']+|"([^"]*)"|'([^']*)'/g) as string[];
+    const playerDetailsArrayRaw = player.match(
+      /[^\s"']+|"([^"]*)"|'([^']*)'/g
+    ) as string[];
     const playerDetails = playerDetailsArrayRaw.map(string =>
       string.replace(/"/g, '')
     );
@@ -367,4 +255,8 @@ function getPlayers(body: string): PlayerObject[] {
       ping: playerDetails[5],
     };
   });
+}
+
+function getServerDetails(rawStatus: string): ServerDetails {
+  return { map: 'cp_sunshine', players: '0/24' };
 }
