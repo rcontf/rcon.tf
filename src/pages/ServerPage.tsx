24c84c2e6fd00ced7778d5a0d771ea0d0a3596b7
c43721 <rlahman.game@gmail.com>
2020-12-24 01:05:25 -0600

    Use formik for adding a new server, add error for when you're adding a server with same ip

--- src/pages/ServerPage.tsx
+++ src/pages/ServerPage.tsx
@@ -1,23 +1,25 @@
-import React, { useState, FormEvent, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layouts/Layout';
 import Server from '../components/Server';
-
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   addServer,
   fetchServers,
   serverSelector,
 } from '../redux/servers/serverSlice';
+import { useFormik } from 'formik';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -35,24 +37,36 @@ export default function ServerPage() {
 
   const [addServerOpen, setAddServerOpen] = useState(false);
 
-  const handleServerAdd = () => {
-    // e.preventDefault();
-    // if (!serverHostname || !serverIp || !serverPassword || !serverPort) return;
-    // else {
-    //   dispatch(
-    //     addServer({
-    //       hostname: serverHostname!.current!.value,
-    //       ip: serverIp!.current!.value,
-    //       password: serverPassword!.current!.value,
-    //       port: parseInt(serverPort!.current!.value) ?? 27015,
-    //     })
-    //   );
-    // }
-  };
-
   useEffect(() => {
     dispatch(fetchServers());
   }, [dispatch]);
+
+  const formik = useFormik({
+    initialValues: {
+      hostname: '',
+      ip: '',
+      password: '',
+      port: 27015,
+    },
+    validate: values => {
+      const userHasServerIp = servers.allServers.some(
+        server => server.ip === values.ip
+      );
+
+      if (userHasServerIp)
+        formik.setFieldError('ip', 'You already have a server with that IP.');
+    },
+    onSubmit: values => {
+      dispatch(
+        addServer({
+          hostname: values.hostname,
+          ip: values.ip,
+          password: values.password,
+          port: values.port,
+        })
+      );
+    },
+  });
 
   return (
     <Layout>
@@ -97,14 +111,63 @@ export default function ServerPage() {
           <DialogTitle id='alert-dialog-title'>Add a new Server</DialogTitle>
           <DialogContent>
             <DialogContentText id='alert-dialog-description'>
-              Add a server to utilize the feature rich
+              Save your server for easy access and management.
             </DialogContentText>
+            <form id='add-server-form' onSubmit={formik.handleSubmit}>
+              <TextField
+                id='server-hostname'
+                name='server-hostname'
+                label='Alias'
+                helperText='What do you want to call the server?'
+                fullWidth
+                required
+                value={formik.values.hostname}
+                onChange={e => formik.setFieldValue('hostname', e.target.value)}
+              />
+              <TextField
+                id='server-ip'
+                name='server-ip'
+                label='IP'
+                fullWidth
+                required
+                value={formik.values.ip}
+                onChange={e => formik.setFieldValue('ip', e.target.value)}
+                error={formik.touched.ip && Boolean(formik.errors.ip)}
+                helperText={formik.touched.ip && formik.errors.ip}
+              />
+              <TextField
+                id='server-password'
+                label='Rcon Password'
+                name='server-password'
+                fullWidth
+                required
+                value={formik.values.password}
+                onChange={e => formik.setFieldValue('password', e.target.value)}
+              />
+              <TextField
+                id='server-port'
+                name='server-port'
+                label='Server port'
+                fullWidth
+                type='number'
+                defaultValue={27015}
+                value={formik.values.port}
+                onChange={e =>
+                  formik.setFieldValue('port', parseInt(e.target.value))
+                }
+              />
+            </form>
           </DialogContent>
           <DialogActions>
             <Button onClick={() => setAddServerOpen(false)} color='primary'>
               Cancel
             </Button>
-            <Button onClick={() => handleServerAdd()} color='primary' autoFocus>
+            <Button
+              type='submit'
+              form='add-server-form'
+              color='primary'
+              autoFocus
+            >
               Add
             </Button>
           </DialogActions>
