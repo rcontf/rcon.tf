7e00c23e0f8129630e21f19e5e99d065e75dbaa9
c43721 <rlahman.game@gmail.com>
2021-01-26 20:47:10 -0600

    Convert customcommand component to use formik

--- src/components/CustomCommand.tsx
+++ src/components/CustomCommand.tsx
@@ -1,24 +1,22 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import TextField from '@material-ui/core/TextField';
-import { Button, Grid, Typography, Paper, Modal } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
 import axios from 'axios';
+import { useFormik } from 'formik';
 
 const useStyles = makeStyles(theme => ({
   center: {
     textAlign: 'center',
-  },
-  modal: {
-    position: 'absolute',
-    width: 800,
-    height: 800,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    textAlign: 'left',
   },
 }));
 
@@ -27,40 +25,33 @@ export default function CustomCommand() {
   const [open, setOpen] = useState<boolean>(false);
   const [response, setResponse] = useState<string>('');
 
-  const serverIp = useRef<HTMLInputElement>();
-  const serverPassword = useRef<HTMLInputElement>();
-  const serverPort = useRef<HTMLInputElement>();
-  const serverCommand = useRef<HTMLInputElement>();
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    if (!serverCommand || !serverIp || !serverPassword || !serverPort) return;
-    else {
+  const formik = useFormik({
+    initialValues: {
+      ip: '',
+      password: '',
+      port: 27015,
+      command: '',
+    },
+    enableReinitialize: true,
+    onSubmit: values => {
+      console.log(values);
       axios
         .post('/api/execute', {
-          ip: serverIp!.current!.value,
-          password: serverPassword!.current!.value,
-          port: parseInt(serverPort!.current!.value) ?? 27015,
-          command: serverCommand!.current!.value,
+          ip: values.ip,
+          password: values.password,
+          port: values.port,
+          command: values.command,
         })
         .then(({ data }) => {
           setResponse(data.body);
-          handleOpen();
+          setOpen(true);
         });
-    }
-  }
+    },
+  });
 
   return (
     <Grid container justify='center' alignItems='center' direction='column'>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <Typography variant='h5' className={styles.center}>
           Try it out!
         </Typography>
@@ -72,8 +63,10 @@ export default function CustomCommand() {
           placeholder='mge.elo.associates'
           fullWidth
           margin='normal'
-          inputRef={serverIp}
           required
+          value={formik.values.ip}
+          onChange={e => formik.setFieldValue('ip', e.target.value)}
+          error={formik.touched.ip && Boolean(formik.errors.ip)}
         />
         <TextField
           id='server-rcon-password'
@@ -83,7 +76,9 @@ export default function CustomCommand() {
           fullWidth
           margin='normal'
           required
-          inputRef={serverPassword}
+          value={formik.values.password}
+          onChange={e => formik.setFieldValue('password', e.target.value)}
+          error={formik.touched.password && Boolean(formik.errors.password)}
         />
         <TextField
           id='server-rcon-port'
@@ -94,7 +89,9 @@ export default function CustomCommand() {
           type='number'
           defaultValue={27015}
           margin='normal'
-          inputRef={serverPort}
+          value={formik.values.port}
+          onChange={e => formik.setFieldValue('port', parseInt(e.target.value))}
+          error={formik.touched.port && Boolean(formik.errors.port)}
         />
         <TextField
           id='server-command'
@@ -104,25 +101,36 @@ export default function CustomCommand() {
           fullWidth
           margin='normal'
           required
-          inputRef={serverCommand}
+          value={formik.values.command}
+          onChange={e => formik.setFieldValue('command', e.target.value)}
+          error={formik.touched.command && Boolean(formik.errors.command)}
         />
 
-        <Button variant='text' type='submit'>
+        <Button variant='outlined' type='submit'>
           Send!
         </Button>
       </form>
 
-      <Modal
+      <Dialog
+        maxWidth='lg'
+        fullWidth
         open={open}
-        onClose={handleClose}
-        aria-labelledby='simple-modal-title'
-        aria-describedby='simple-modal-description'
+        onClose={() => setOpen(false)}
+        aria-labelledby='rcon-response-dialog'
+        aria-describedby='rcon-response-dialog'
       >
-        <Paper className={styles.modal}>
-          <h2 id='simple-modal-title'>Response</h2>
+        <DialogTitle id='rcon-response-dialog-title'>
+          Server's Response
+        </DialogTitle>
+        <DialogContent>
           <pre>{response}</pre>
-        </Paper>
-      </Modal>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color='primary'>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 }
