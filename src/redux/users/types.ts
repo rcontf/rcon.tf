85f068304455491d8e3ee8ffdff460842c8263ff
c43721 <rlahman.game@gmail.com>
2021-02-28 15:43:19 -0600

    Refresh token upon successful authentication

--- src/redux/users/types.ts
+++ src/redux/users/types.ts
@@ -1,4 +1,12 @@
+export enum Roles {
+    ADMIN = "admin",
+    USER = "user"
+}
+
 export interface UserData {
+    loggedIn: boolean;
+
+    // roles: Roles[]
     avatar: string;
     id: string;
     name: string;
