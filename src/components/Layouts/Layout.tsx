b14e44a2cffdb41aaee872aee3527f9cbe89466e
c43721 <rlahman.game@gmail.com>
2021-03-22 15:07:48 -0500

    Allow for setting custom document title

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 // import Footer from './Footer';
 
 interface LayoutProps {
   children?: React.ReactNode;
+  title?: string;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, title }: LayoutProps) {
   const login = () => window.location.replace('/api/auth/steam');
   const logout = () => window.location.replace('/api/auth/logout');
+
+  useEffect(() => {
+    document.title = title ?? 'rcon.tf';
+  }, []);
 
   return (
     <>
